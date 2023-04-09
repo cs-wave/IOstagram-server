7590d47cb66b2a44ad9c4bd6489ba4f2272deabf
slo0ey <rurchi1206@gmail.com>
2023-04-09 22:26:44 +0900

    Setup: 프로젝트 생성

---
+++ src/main/kotlin/com/github/cswave/Application.kt
@@ -0,0 +1,18 @@
+package com.github.cswave
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.github.cswave.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
